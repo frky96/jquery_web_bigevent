2c9a9ef7b9fe20dbd43b3a23e5b6dc91799d5a8c
frky96 <2787018357@qq.com>
2022-01-11 22:43:31 +0800

    index session completed

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,5 +3,18 @@
 //  这个函数可以传入ajax中的参数
 
 $.ajaxPrefilter(function (params) {
+  
   params.url = 'http://www.liulongbin.top:3007' + params.url;
+
+  if (params.url.indexOf('/my/') !== -1) {
+    params.headers = {Authorization: localStorage.getItem('token') || ''};
+  }
+
+  params.complete = function (res) {
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+      localStorage.removeItem('token');
+      location.href = '/login.html';
+    }
+  }
+
 })
